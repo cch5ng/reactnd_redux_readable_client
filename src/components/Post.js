ce32ba8efd15d36da5d9d60a1466962d17653f7e
Carol Chung <cch5ng@gmail.com>
2017-08-29 23:20:48 -0700

    fix: #21; moved <Link> to fields not for voteScore

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,13 +9,15 @@ class Post extends Component {
 
   render() {
     const post = this.props.post
+    const link = `/posts/${post.id}`
+
     return (
-      <li key={post.id} className="post-list-item">
-        {post.title}<br />
-        Author: {post.author}<br />
+      <div key={post.id} className="post-list-item">
+        <Link to={link} >{post.title}</Link><br />
+        <Link to={link} >Author: {post.author}</Link><br />
         Votes: {post.voteScore} <ArrowUpIcon className="post-arrow-up-icon" onClick={(ev) => this.props.clickVote(ev, post.id)} /><ArrowDownIcon className="post-arrow-down-icon"  onClick={(ev) => this.props.clickVote(ev, post.id)} /><br />
-        Time: {this.props.prettyTime(post.timestamp)}<br />
-      </li>
+        <Link to={link} >Time: {this.props.prettyTime(post.timestamp)}</Link><br />
+      </div>
     )
   }
 }
