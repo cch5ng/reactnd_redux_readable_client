6dc89502935efe5ba084c179b59b8fe7a2a51cb6
Carol Chung <cch5ng@gmail.com>
2017-08-30 09:57:20 -0700

    cleanup; did not need connect

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-//import { connect } from 'react-redux'
+import { REQUEST_POST_DETAIL, RECEIVE_POST_DETAIL, fetchPostDetail } from '../actions'
 import ArrowDownIcon from 'react-icons/lib/fa/arrow-circle-down'
 import ArrowUpIcon from 'react-icons/lib/fa/arrow-circle-up'
 import '../App.css';
@@ -9,24 +9,17 @@ class Post extends Component {
 
   render() {
     const link = `/posts/${this.props.post.id}`
+    const post = this.props.post
 
     return (
       <div className="post-list-item">
-        <Link to={link} >{this.props.post.title}</Link><br />
-        Author: {this.props.post.author}<br />
-        Votes: {this.props.post.voteScore} <ArrowUpIcon className="post-arrow-up-icon" onClick={(ev) => this.props.clickVote(ev, this.props.post.id)} /><ArrowDownIcon className="post-arrow-down-icon"  onClick={(ev) => this.props.clickVote(ev, this.props.post.id)} /><br />
-        Time: {this.props.prettyTime(this.props.post.timestamp)}<br />
+        <Link to={link} >{post.title}</Link><br />
+          Author: {post.author}<br />
+          Votes: {post.voteScore} <ArrowUpIcon className="post-arrow-up-icon" onClick={(ev) => this.props.clickVote(ev, post.id)} /><ArrowDownIcon className="post-arrow-down-icon"  onClick={(ev) => this.props.clickVote(ev, post.id)} /><br />
+          Time: {this.props.prettyTime(post.timestamp)}<br /> 
       </div>
     )
   }
 }
-
-// function mapStateToProps({ }) {
-
-//   return {
-//   }
-// }
-
-// export default connect(mapStateToProps)(Post);
 
 export default Post
